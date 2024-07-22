4929f5af350ab58f327625e9c279348868d1204c
Azumara Joseph <azumarajoseph@gmail.com>
2024-07-22 13:23:00 +0100

    Update code

--- src/App.js
+++ src/App.js
@@ -4,118 +4,52 @@ import Hero from './components/Hero';
 import Card from './components/card';
 
 export default function App() {
+
+  const colors = [<h2>red</h2>, <h2>brown</h2>, <h2>white</h2>]
+  // const cards = colors.map(color => `<h3>${color}</h3>`)
+
+
+  
+
   return (
     <div>
       <NavBar />
+      {colors}
       <Hero />
       <Card img='/img/katie-zaferes.png'
             rating= '5.0'
             count= {6}
-            country='Online'
+            location='Online'
             title='Life Lessons with Katie Zaferes'
             description='I will share with you what I call "Positively Impactful Moments of Disappointment." Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.'
             price={136}
+            openSpots= {0}
+
             />
       <Card img='/img/wedding-photography.png'
             rating= '5.0'
             count= {30}
-            country='Online'
+            location='Online'
             title='Learn Wedding Photography'
             description='Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that will last a lifetime.'
             price={125}
+            openSpots= {27}
+
       />
-      <Card />
-      <Card />
-      <Card />
+      <Card img='/img/mountain-bike.png'
+            rating= '4.5'
+            count= {2}
+            location='Norway'
+            title='Group Mountain Biking'
+            description='Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)'
+            price={50}
+            openSpots= {3}
+            
+      />
     </div>
   );
 
-//   const data = [
-//     {
-//         id: 1,
-//         title: "Life Lessons with Katie Zaferes",
-//         description:
-//             'I will share with you what I call "Positively Impactful Moments of Disappointment." Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.',
-//         price: 136,
-//         coverImg: require("./images/katie-zaferes.png"),
-//         stats: {
-//             rating: 5.0,
-//             reviewCount: 6,
-//         },
-//         location: "Online",
-//         openSpots: 0,
-//     },
-//     {
-//         id: 2,
-//         title: "Learn Wedding Photography",
-//         description:
-//             "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
-//         price: 125,
-//         coverImg: require("./images/wedding-photography.png"),
-//         stats: {
-//             rating: 5.0,
-//             reviewCount: 30,
-//         },
-//         location: "Online",
-//         openSpots: 27,
-//     },
-//     {
-//         id: 3,
-//         title: "Group Mountain Biking",
-//         description:
-//             "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
-//         price: 50,
-//         coverImg: require("./images/mountain-bike.png"),
-//         stats: {
-//             rating: 4.8,
-//             reviewCount: 2,
-//         },
-//         location: "Norway",
-//         openSpots: 3,
-//     },
-//     {
-//         id: 4,
-//         title: "Life Lessons with Katie Zaferes",
-//         description:
-//             'I will share with you what I call "Positively Impactful Moments of Disappointment." Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.',
-//         price: 136,
-//         coverImg: require("./images/katie-zaferes.png"),
-//         stats: {
-//             rating: 5.0,
-//             reviewCount: 6,
-//         },
-//         location: "Online",
-//         openSpots: 0,
-//     },
-//     {
-//         id: 5,
-//         title: "Learn Wedding Photography",
-//         description:
-//             "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
-//         price: 125,
-//         coverImg: require("./images/wedding-photography.png"),
-//         stats: {
-//             rating: 5.0,
-//             reviewCount: 30,
-//         },
-//         location: "Online",
-//         openSpots: 27,
-//     },
-//     {
-//         id: 6,
-//         title: "Group Mountain Biking",
-//         description:
-//             "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
-//         price: 50,
-//         coverImg: require("./images/mountain-bike.png"),
-//         stats: {
-//             rating: 4.8,
-//             reviewCount: 2,
-//         },
-//         location: "Norway",
-//         openSpots: 3,
-//     },
-// ];
+//   
 
 // export default data;
 
--- src/components/card.js
+++ src/components/card.js
@@ -2,6 +2,24 @@ import React from "react";
 // import KatieLogo from "../img/katie-zaferes.png"
 
 export default function Card(props) {
+
+    const nums = [1, 2, 3, 4, 5, 6]
+    const numSquare = nums.map(num => num * num)
+    // console.log(numSquare);
+
+    const names = ['john', 'charles', 'joseph', 'kalu']
+    const capitalized = names.map(name => {
+        return name[0].toUpperCase() + name.slice(1)
+
+    })
+// console.log(capitalized);
+
+const pokesMan = ["Chander", "Justin", "Austin"]
+const poker = pokesMan.map(poke => {
+    return `<p>${poke}</p>`
+})
+//   console.log(poker);
+
     return (
         <div className="card">
             <img src={props.img} alt="Katie Logo" className="card-image" />
@@ -10,8 +28,8 @@ export default function Card(props) {
                     star
                 </span>
                 <span>{props.rating}</span>
-                <span className="grey">(props.count) .&nbsp;</span>
-                <span className="grey"> {props.contry}</span>
+                <span className="grey">({props.count}) .&nbsp;</span>
+                <span className="grey"> {props.location}</span>
             </div>
             <p>{props.title}</p>
             <p><strong>From ${props.price}</strong> / person</p>
